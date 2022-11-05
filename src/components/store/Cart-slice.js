3c5a95082e219ecd6101cd0418c85e5875ad2bd8
Mokány Ádám <mokanyadam@gmail.com>
2022-11-05 10:55:41 +0100

    - small error correction in the cart

--- src/components/store/Cart-slice.js
+++ src/components/store/Cart-slice.js
@@ -11,7 +11,7 @@ const cartSlice = createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;   // A "newItem" egyenlő lesz az "action"-ből érkező adatokkal.
       const existingItem = state.items.find((item) => item.id === newItem.id);  // Már a kosárban létező elem. Ugye az "items" tömbömben megnézem (find), hogy van-e olyan termék azonosító (item.id), ami megyegyezik az új termékem azonosítójával (newItem.id).
-      state.totalQuantity++;
+
       if (!existingItem) {  // Ha nincs ilyen (nem része a tömbömnek)...
         state.items.push({  // ...akkor az "items" tömbömbe felveszem egy új termékként, megadva neki az összes szükséges paramétert, ami rajtad múlik, hogy miket szeretnél hozzáadni.
           id: newItem.id,
@@ -20,11 +20,12 @@ const cartSlice = createSlice({
           totalPrice: newItem.price,
           name: newItem.title
         });
+        state.totalQuantity++;
       }else{    // Ha ez a termék már létezik a kosárban, akkor frissíteni szeretném a terméket.
         // Ugye az "itemId" a "price" és a "name" nem változik.
         existingItem.quantity++;    // A teljes mennyiséget kell megpluszolni egyet a termékből. 
         existingItem.totalPrice = existingItem.totalPrice + newItem.price   // És az eddigi teljes összeghez kell hozzáadnunk a termék árát.
-        state.totalQuantity--;
+        state.totalQuantity++;
       }
     },
     // Kosárból törlés
@@ -34,10 +35,12 @@ const cartSlice = createSlice({
         if(existingItem.quantity === 1){    // Ha a létező termék mennyiség egyenlő egyel, akkor el kell távolítani a teljes elemet a tömmből.
             //  A meglévő elemek listájából kiszűrjük (filter()) azt az elemet, amelyik el akarjuk távolítani.
             //  Ez felülírja az elemek tömbjét egy új tömbbel, ahol ez az elem, amelyiket el akarunk távolítnai, hiányozni fog.
-            state.items = state.items.filter(item => item.id !== id)    // Azok az elemek, amik nem egyenlőek a keresett termékkel (id) visszaadja őket egy új tömbben, amit ugye feljebb is írtam. A többit nem adja vissza, ergo törölve lesznek.  
+            state.items = state.items.filter(item => item.id !== id)    // Azok az elemek, amik nem egyenlőek a keresett termékkel (id) visszaadja őket egy új tömbben, amit ugye feljebb is írtam. A többit nem adja vissza, ergo törölve lesznek.
+            state.totalQuantity--;  
         }else{  // Ha nagyobb egynél...
             existingItem.quantity--;    // ...akkor csak csökkenteni szeretnénk a mennyiségét egyel.
             existingItem.totalPrice = existingItem.totalPrice - existingItem.price; // És csökkent kell a teljes fizetendö összeget is az eltávolított termék összegével.
+            state.totalQuantity--;
         }
     },
   },
