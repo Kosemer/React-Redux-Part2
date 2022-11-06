2baa7609864bbd0f58aa3b6b0a61db3f3aa71ab9
Mokány Ádám <mokanyadam@gmail.com>
2022-11-06 21:48:05 +0100

    - Error handling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
+import { uiSliceActions } from "./components/store/UI-slice";
+import { Fragment } from "react";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;   // Ez a változó arra kell, hogy ne jelenítsen meg az oldal betöltésekor 'Success' üzenetet felül. Az 'App'-on kívül kell definiálni, hogy újrafutáskor(useEffect) ne értékelje ki.
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
-
+  const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
 
   useEffect(() => {
-    fetch("https://productsredux-4f384-default-rtdb.firebaseio.com/cart.json", {
-      method: "PUT",
-      body: JSON.stringify(cart)
-    }); // A 'PUT' küldés annyiban másabb mint a 'POST', hogy az új adatok (amiket küldünk) felülírják a már meglévőket az adatbázisban.
-  }, [cart]); // És kell hozzá a függőség is, hogy a 'useEffect' újra lefusson, amikor a kosár változik.
+    const sendCartData = async () => {
+      const response = await fetch(
+        "https://productsredux-4f384-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed!");
+      }
+
+      dispatch(
+        uiSliceActions.showNotification({
+          status: "success",
+          title: "Success",
+          message: "Send cart data successfully!",
+        })
+      );
+    }; // A 'PUT' küldés annyiban másabb mint a 'POST', hogy az új adatok (amiket küldünk) felülírják a már meglévőket az adatbázisban.
+    if(isInitial){  //  Ha ez 'true' akkor ne hajtsa végre a catch ágat. De egyben beállítom 'false'-ra, hogy újraértékeléskor már jelenítse meg a catch ágat.
+      isInitial = false;
+      return;
+    }
+    sendCartData().catch((error) => {
+      dispatch(
+        uiSliceActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Sending cart data failed!",
+        })
+      );
+    });
+  }, [cart, dispatch]); // És kell hozzá a függőség is, hogy a 'useEffect' újra lefusson, amikor a kosár változik.
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        ></Notification>
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/components/store/UI-slice.js
+++ src/components/store/UI-slice.js
@@ -1,14 +1,21 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlicec = createSlice({
-    name: 'ui',
-    initialState: {cartIsVisible: true},
-    reducers: {
-        toggle(state) {
-            state.cartIsVisible = !state.cartIsVisible
-        }
-    }
-})
+  name: "ui",
+  initialState: { cartIsVisible: true, notification: null },
+  reducers: {
+    toggle(state) {
+      state.cartIsVisible = !state.cartIsVisible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
+});
 
 export default uiSlicec;
 
